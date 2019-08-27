7a1f909328f056722852f5926f3232722fe5195e
Technote <technote.space@gmail.com>
2019-08-28 01:03:21 +0900

    feat: custom config name (#3)
    (cherry picked from commit 4dcb392a346b6caba1908f8a1d4745bbd6b4d789)

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 export const CONFIG_PATH = '.github';
-export const CONFIG_FILENAME = 'card-labeler.yml';
+export const DEFAULT_CONFIG_FILENAME = 'card-labeler.yml';
 export const TARGET_EVENT_NAME = 'project_card';
 export const TARGET_EVENT_ACTION = 'moved';
