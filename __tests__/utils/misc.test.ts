ab2ccb79e8703209a9746535dd039172b99dbb24
Technote <technote.space@gmail.com>
2019-09-29 23:26:43 +0900

    feat: use library (#49, #46, #45)

--- __tests__/utils/misc.test.ts
+++ __tests__/utils/misc.test.ts
@@ -1,136 +1,116 @@
+/* eslint-disable no-magic-numbers */
 import nock from 'nock';
 import path from 'path';
-import {GitHub} from '@actions/github' ;
-import {disableNetConnect, encodeContent, getApiFixture, getContext} from '../util';
-import {isTargetEvent, parseConfig, getProjectName, getColumnName, getConfigFilename, getBuildVersion} from '../../src/utils/misc';
-import {DEFAULT_CONFIG_FILENAME} from '../../src/constant';
+import { GitHub } from '@actions/github' ;
+import { isTargetEvent } from '@technote-space/filter-github-action';
+import { disableNetConnect, getApiFixture, getContext } from '@technote-space/github-action-test-helper';
+import { getProjectName, getColumnName, getConfigFilename } from '../../src/utils/misc';
+import { TARGET_EVENTS, DEFAULT_CONFIG_FILENAME } from '../../src/constant';
 
 describe('isTargetEvent', () => {
-    it('should return true', () => {
-        expect(isTargetEvent(getContext({
-            payload: {
-                action: 'moved',
-            },
-            eventName: 'project_card',
-        }))).toBeTruthy();
-    });
+	it('should return true', () => {
+		expect(isTargetEvent(TARGET_EVENTS, getContext({
+			payload: {
+				action: 'moved',
+			},
+			eventName: 'project_card',
+		}))).toBeTruthy();
+	});
 
-    it('should return false', () => {
-        expect(isTargetEvent(getContext({
-            payload: {
-                action: 'moved',
-            },
-            eventName: 'push',
-        }))).toBeFalsy();
-    });
+	it('should return false', () => {
+		expect(isTargetEvent(TARGET_EVENTS, getContext({
+			payload: {
+				action: 'moved',
+			},
+			eventName: 'push',
+		}))).toBeFalsy();
+	});
 
-    it('should return false', () => {
-        expect(isTargetEvent(getContext({
-            payload: {
-                action: 'created',
-            },
-            eventName: 'project_card',
-        }))).toBeFalsy();
-    });
-});
-
-describe('parseConfig', () => {
-    it('should parse config', async () => {
-        expect(parseConfig(encodeContent(''))).toEqual({});
-        expect(parseConfig(encodeContent('a: b'))).toEqual({a: 'b'});
-        expect(parseConfig(encodeContent('a:\n  - b\n  - c'))).toEqual({a: ['b', 'c']});
-    });
+	it('should return false', () => {
+		expect(isTargetEvent(TARGET_EVENTS, getContext({
+			payload: {
+				action: 'created',
+			},
+			eventName: 'project_card',
+		}))).toBeFalsy();
+	});
 });
 
 describe('getProjectName', () => {
-    disableNetConnect(nock);
+	disableNetConnect(nock);
 
-    it('should return project name', async () => {
-        nock('https://api.github.com')
-            .get('/projects/1')
-            .reply(200, getApiFixture('projects.get'));
+	it('should return project name', async() => {
+		nock('https://api.github.com')
+			.get('/projects/1')
+			.reply(200, getApiFixture(path.resolve(__dirname, '..', 'fixtures'), 'projects.get'));
 
-        expect(await getProjectName(1, new GitHub(''))).toBe('Projects Documentation');
-    });
+		expect(await getProjectName(1, new GitHub(''))).toBe('Backlog');
+	});
 
-    it('should not return project name', async () => {
-        nock('https://api.github.com')
-            .get('/projects/1')
-            .reply(404);
-        const fn = jest.fn();
+	it('should not return project name', async() => {
+		nock('https://api.github.com')
+			.get('/projects/1')
+			.reply(404);
+		const fn = jest.fn();
 
-        try {
-            await getProjectName(1, new GitHub(''));
-        } catch (error) {
-            fn();
-            expect(error).toHaveProperty('status');
-            expect(error.status).toBe(404);
-        }
-        expect(fn).toBeCalled();
-    });
+		try {
+			await getProjectName(1, new GitHub(''));
+		} catch (error) {
+			fn();
+			expect(error).toHaveProperty('status');
+			expect(error.status).toBe(404);
+		}
+		expect(fn).toBeCalled();
+	});
 });
 
 describe('getColumnName', () => {
-    disableNetConnect(nock);
+	disableNetConnect(nock);
 
-    it('should return column name', async () => {
-        nock('https://api.github.com')
-            .get('/projects/columns/1')
-            .reply(200, getApiFixture('projects.columns'));
+	it('should return column name', async() => {
+		nock('https://api.github.com')
+			.get('/projects/columns/1')
+			.reply(200, getApiFixture(path.resolve(__dirname, '..', 'fixtures'), 'projects.columns'));
 
-        expect(await getColumnName(1, new GitHub(''))).toBe('To Do');
-    });
+		expect(await getColumnName(1, new GitHub(''))).toBe('To Do');
+	});
 
-    it('should not return column name', async () => {
-        nock('https://api.github.com')
-            .get('/projects/columns/1')
-            .reply(404);
-        const fn = jest.fn();
+	it('should not return column name', async() => {
+		nock('https://api.github.com')
+			.get('/projects/columns/1')
+			.reply(404);
+		const fn = jest.fn();
 
-        try {
-            await getColumnName(1, new GitHub(''));
-        } catch (error) {
-            fn();
-            expect(error).toHaveProperty('status');
-            expect(error.status).toBe(404);
-        }
-        expect(fn).toBeCalled();
-    });
+		try {
+			await getColumnName(1, new GitHub(''));
+		} catch (error) {
+			fn();
+			expect(error).toHaveProperty('status');
+			expect(error.status).toBe(404);
+		}
+		expect(fn).toBeCalled();
+	});
 });
 
 describe('getConfigFilename', () => {
-    const OLD_ENV = process.env;
+	const OLD_ENV = process.env;
 
-    beforeEach(() => {
-        jest.resetModules();
-        process.env = {...OLD_ENV};
-        delete process.env.NODE_ENV;
-    });
+	beforeEach(() => {
+		jest.resetModules();
+		process.env = {...OLD_ENV};
+		delete process.env.NODE_ENV;
+	});
 
-    afterEach(() => {
-        process.env = OLD_ENV;
-    });
+	afterEach(() => {
+		process.env = OLD_ENV;
+	});
 
-    it('should get config filename', () => {
-        process.env.INPUT_CONFIG_FILENAME = 'test';
-        expect(getConfigFilename()).toBe('test');
-    });
+	it('should get config filename', () => {
+		process.env.INPUT_CONFIG_FILENAME = 'test';
+		expect(getConfigFilename()).toBe('test');
+	});
 
-    it('should get default config filename', () => {
-        expect(getConfigFilename()).toBe(DEFAULT_CONFIG_FILENAME);
-    });
-});
-
-describe('getBuildVersion', () => {
-    it('should get build version', () => {
-        expect(getBuildVersion(path.resolve(__dirname, '..', 'fixtures', 'build1.json'))).toBe('v1.2.3');
-    });
-
-    it('should return false 1', () => {
-        expect(getBuildVersion(path.resolve(__dirname, '..', 'fixtures', 'build2.json'))).toBeFalsy();
-    });
-
-    it('should return false 2', () => {
-        expect(getBuildVersion(path.resolve(__dirname, '..', 'fixtures', 'build.test.json'))).toBeFalsy();
-    });
+	it('should get default config filename', () => {
+		expect(getConfigFilename()).toBe(DEFAULT_CONFIG_FILENAME);
+	});
 });
--- src/utils/label.ts
+++ src/utils/label.ts
@@ -1,9 +1,9 @@
-import {flatMap, uniq, difference, intersection} from 'lodash';
+import { flatMap, uniq, difference, intersection } from 'lodash';
 
-const getLabels = (config: object, project: string, column: string) => column in config[project] ? ('object' === typeof config[project][column] ? uniq(Object.values(config[project][column])) : [config[project][column]]) : [];
+const getLabels = (config: object, project: string, column: string): string[] => column in config[project] ? ('object' === typeof config[project][column] ? uniq(Object.values(config[project][column])) : [config[project][column]]) : [];
 
-const getProjectLabels = (config: object, project: string) => uniq(flatMap(Object.keys(config[project]), column => getLabels(config, project, column)));
+const getProjectLabels = (config: object, project: string): string[] => uniq(flatMap(Object.keys(config[project]), column => getLabels(config, project, column)));
 
-export const getRemoveLabels = (currentLabels: string[], project: string, column: string, config: object) => intersection(currentLabels, difference(getProjectLabels(config, project), getAddLabels([], project, column, config)));
+export const getAddLabels = (currentLabels: string[], project: string, column: string, config: object): string[] => difference(getLabels(config, project, column), currentLabels);
 
-export const getAddLabels = (currentLabels: string[], project: string, column: string, config: object) => difference(getLabels(config, project, column), currentLabels);
+export const getRemoveLabels = (currentLabels: string[], project: string, column: string, config: object): string[] => intersection(currentLabels, difference(getProjectLabels(config, project), getAddLabels([], project, column, config)));
